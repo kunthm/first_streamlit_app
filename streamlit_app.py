16a66d537b92670a8f8e3bb9847ffbbe70536f0a
kunthm <91789562+kunthm@users.noreply.github.com>
2023-07-11 16:48:14 -0500

    Update streamlit_app.py
    error update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('Breakefast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
-treamlit.text('Kale , spinach & Rocket smoothie')
+treamlit.text('Kale ,spinach & Rocket smoothie')
 treamlit.text('Hard-Boiled free_range chicken')
